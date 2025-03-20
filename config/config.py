7d259b31580301b642d2c725b84d7171f6b33c43
ShMelL25 <108915857+ShMelL25@users.noreply.github.com>
2025-03-20 11:07:06 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = '6010034078:AAG7v87SYEYrsDgFNKrjJrigbRJR2fZekdg'
+BOT_TOKEN = ''
 #SQL_URL = "postgresql+psycopg2://postgres:Santaclausoffice6@192.168.31.230:5432/trade_bd"
-SQL_URL = "postgresql+psycopg2://postgres:Santaclausoffice6@127.0.0.1:5432/trade_bd"
+SQL_URL = "postgresql+psycopg2://postgres:@127.0.0.1:5432/trade_bd"
 
