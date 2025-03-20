91f9a95f0e238990ad5aba07f3782287e46d9dfd
ShMelL25 <108915857+ShMelL25@users.noreply.github.com>
2025-03-20 11:07:27 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = ''
-#SQL_URL = "postgresql+psycopg2://postgres:Santaclausoffice6@192.168.31.230:5432/trade_bd"
+#SQL_URL = "postgresql+psycopg2://postgres:@192.168.31.230:5432/trade_bd"
 SQL_URL = "postgresql+psycopg2://postgres:@127.0.0.1:5432/trade_bd"
 
