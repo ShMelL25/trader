aa63e751b6836e07c4f7d556946bc04bec667d0c
Shchemelyov Egor <egor2014ag@mail.ru>
2025-05-14 22:43:58 +0300

    edit config

--- config/config.py
+++ config/config.py
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 BOT_TOKEN = ''
 #SQL_URL = "postgresql+psycopg2://postgres:@192.168.31.230:5432/trade_bd"
 SQL_URL = "postgresql+psycopg2://postgres:@127.0.0.1:5432/trade_bd"
-=======
+
 BOT_TOKEN = '6010034078:AAG7v87SYEYrsDgFNKrjJrigbRJR2fZekdg'
-SQL_URL = "postgresql+psycopg2://postgres:Santaclausoffice6@192.168.31.230:5432/trade_bd"
+#SQL_URL = "postgresql+psycopg2://postgres:Santaclausoffice6@192.168.31.230:5432/trade_bd"
 #SQL_URL = "postgresql+psycopg2://postgres:Santaclausoffice6@127.0.0.1:5432/trade_bd"
->>>>>>> 01a6d4a (edit_critical_error)
+
 
